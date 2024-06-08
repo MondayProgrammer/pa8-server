70199c197e3baa2b9022ac9d5709e93ae0ede4ca
Jean-François Simonnet <monday.programmer@gmail.com>
2024-06-08 03:23:11 +0200

    move web

--- thebestdeal.sh
+++ thebestdeal.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 migrate -path ./migrations -database $$THEBESTDEAL_DB_URL up 
+sudo mv ./bin/linux_amd64/web ~
+
 sudo mv ./service/web.service /etc/systemd/system/ 
 sudo restorecon -v /etc/systemd/system/web.service
 sudo systemctl daemon-reload
