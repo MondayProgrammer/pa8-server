d18b8670e7ba57c0ba06e3248fb7afb20d0aea1c
Jean-François Simonnet <monday.programmer@gmail.com>
2024-06-08 15:47:20 +0200

    update

--- thebestdeal.sh
+++ thebestdeal.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-migrate -path ./migrations -database $$THEBESTDEAL_DB_URL up 
+#migrate -path ./migrations -database $THEBESTDEAL_DB_URL up 
+mysql -D thebestdeal --user=$THEBESTDEAL_DB_USER --password=$THEBESTDEAL_DB_PASSWORD < ./migrations/000001_create_tables.up.sql
 sudo mv ./bin/linux_amd64/web ~
 
 sudo mv ./service/web.service /etc/systemd/system/ 
